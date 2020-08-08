1705992aba68f1dc37fc2b8df2319a13b74d6535
Astrayesa <jsparrow0404@gmail.com>
2020-08-08 22:39:19 +0700

    matrix 3x3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,40 @@
-function input_setup(ordo){
-    let input_table = document.getElementById('input-table');
-    input_table.innerHTML = "";
-    let inp_field = '<input type="text" class="matrix-data">';
-    for (let j = 0; j < ordo; j++) {
-        let table_row = document.createElement("tr");
-        for (let i = 0; i < ordo; i++) {
-            let table_data = document.createElement("td");
-            table_data.innerHTML = inp_field;
-            table_row.appendChild(table_data);
-        }
-        input_table.appendChild(table_row);
-    }
+// function input_setup(ordo){
+//     let input_table = document.getElementById('input-table');
+//     input_table.innerHTML = "";
+//     let inp_field = '<input type="text" class="matrix-data">';
+//     for (let j = 0; j < ordo; j++) {
+//         let table_row = document.createElement("tr");
+//         for (let i = 0; i < ordo; i++) {
+//             let table_data = document.createElement("td");
+//             table_data.innerHTML = inp_field;
+//             table_row.appendChild(table_data);
+//         }
+//         input_table.appendChild(table_row);
+//     }
+// }
+
+function deter(arr){
+    det = arr[0] * arr[2] - arr[1] * arr[2];
+    return det;
 }
 
-const genenerate_button = document.getElementById("generate-button");
-genenerate_button.addEventListener('click', function(event){
-    let ordo_number = document.getElementById("ordo-number");
-    input_setup(ordo_number.value);
+const inverse_button = document.getElementById("inverse-button");
+inverse_button.addEventListener('click', function(event){
+    let matrix_data = document.getElementsByClassName("matrix-data");
+    let determinan =  matrix_data[0].value * matrix_data[4].value * matrix_data[8].value
+                    + matrix_data[1].value * matrix_data[5].value * matrix_data[6].value
+                    + matrix_data[2].value * matrix_data[3].value * matrix_data[7].value
+                    - matrix_data[2].value * matrix_data[4].value * matrix_data[6].value
+                    - matrix_data[0].value * matrix_data[5].value * matrix_data[7].value
+                    - matrix_data[1].value * matrix_data[3].value * matrix_data[8].value;
+    alert(determinan);
+    kofaktor = new Array(3);
+    for (let i = 0; i < 3; i++){
+        kofaktor[i] = new Array(3);
+        for(let j = 0; j < 3; j++){
+        }
+    }
 });
 
 
-input_setup(3);
+// input_setup(3);
