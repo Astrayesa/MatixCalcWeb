3b7a2a1be7c966c13b913c71395cbf1474c4f1de
Astrayesa <jsparrow0404@gmail.com>
2020-08-11 10:27:03 +0700

    main calculation finish

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -1,25 +1,25 @@
-function inverse(){
-    let matrix_data = document.getElementsByClassName("matrix-data");
-    let ordo = document.getElementById("ordo-number").value;
-    let matrix = new Array(ordo);
+// function inverse(){
+//     let matrix_data = document.getElementsByClassName("matrix-data");
+//     let ordo = document.getElementById("ordo-number").value;
+//     let matrix = new Array(ordo);
 
-    for(let i = 0; i < ordo; i++){
-        matrix[i] = new Array(ordo);
-    }
+//     for(let i = 0; i < ordo; i++){
+//         matrix[i] = new Array(ordo);
+//     }
 
-    let k = 0;
-    for (let i = 0; i < ordo; i++) {
-        for (let j = 0; j < ordo; j++) {
-            matrix[i][j] = matrix_data[k].value;
-            k++;
-        }
+//     let k = 0;
+//     for (let i = 0; i < ordo; i++) {
+//         for (let j = 0; j < ordo; j++) {
+//             matrix[i][j] = matrix_data[k].value;
+//             k++;
+//         }
         
-    }
-    for (let i = 0; i < ordo; i++) {
-        for (let j = 0; j < ordo; j++) {
-            console.log(matrix[i][j]);
-            console.log("   ")
-        }
-        console.log('\n');
-    }
-}
+//     }
+//     for (let i = 0; i < ordo; i++) {
+//         for (let j = 0; j < ordo; j++) {
+//             console.log(matrix[i][j]);
+//             console.log("   ")
+//         }
+//         console.log('\n');
+//     }
+// }
--- assets/js/main.js
+++ assets/js/main.js
@@ -13,28 +13,74 @@
 //     }
 // }
 
+function printInverse(invarr){
+    let input_table = document.getElementById('output-table');
+    input_table.innerHTML = "";
+    for (let j = 0; j < 3; j++) {
+        let table_row = document.createElement("tr");
+        for (let i = 0; i < 3; i++) {
+            let table_data = document.createElement("td");
+            table_data.innerHTML = invarr[j][i];
+            table_row.appendChild(table_data);
+        }
+        input_table.appendChild(table_row);
+    }
+}
+
+function toidx(a, b){
+    a += 1;
+    b += 1;
+    return 3 * (a - 1) + b - 1;
+}
+
 function deter(arr){
-    det = arr[0] * arr[2] - arr[1] * arr[2];
+    det = arr[0] * arr[3] - arr[1] * arr[2];
     return det;
 }
 
 const inverse_button = document.getElementById("inverse-button");
 inverse_button.addEventListener('click', function(event){
     let matrix_data = document.getElementsByClassName("matrix-data");
-    let determinan =  matrix_data[0].value * matrix_data[4].value * matrix_data[8].value
-                    + matrix_data[1].value * matrix_data[5].value * matrix_data[6].value
-                    + matrix_data[2].value * matrix_data[3].value * matrix_data[7].value
-                    - matrix_data[2].value * matrix_data[4].value * matrix_data[6].value
-                    - matrix_data[0].value * matrix_data[5].value * matrix_data[7].value
-                    - matrix_data[1].value * matrix_data[3].value * matrix_data[8].value;
-    alert(determinan);
+    // let determinan =  matrix_data[0].value * matrix_data[4].value * matrix_data[8].value
+    //                 + matrix_data[1].value * matrix_data[5].value * matrix_data[6].value
+    //                 + matrix_data[2].value * matrix_data[3].value * matrix_data[7].value
+    //                 - matrix_data[2].value * matrix_data[4].value * matrix_data[6].value
+    //                 - matrix_data[0].value * matrix_data[5].value * matrix_data[7].value
+    //                 - matrix_data[1].value * matrix_data[3].value * matrix_data[8].value;
+    // alert(determinan);
     kofaktor = new Array(3);
     for (let i = 0; i < 3; i++){
         kofaktor[i] = new Array(3);
         for(let j = 0; j < 3; j++){
+            minor = new Array(4);
+            let ind = 0;
+            for(let k = 0; k < 3; k++){
+                for(let l = 0; l < 3; l++){
+                    if(!(k == i || l == j)){
+                        minor[ind] = parseInt(matrix_data[toidx(k, l)].value);
+                        ++ind;
+                    }
+                }
+            }
+            kofaktor[i][j] = (-1) ** ((i + 1) + (j + 1)) * deter(minor);
         }
     }
+    let determinan = 0;
+    for(let i = 0; i < 3; i++){
+        determinan += kofaktor[i][0] * matrix_data[toidx(i, 0)].value;
+    }
+
+    if(determinan == 0){
+        alert("Determinan = 0, maka matrix tersebut tidak memiliki inverse");
+        return;
+    }
+
+    const inverse = new Array(3);
+    for (let i = 0; i < 3; i++){
+        inverse[i] = new Array(3);
+        for (let j = 0; j < 3; j++){
+            inverse[i][j] = (kofaktor[j][i] / determinan).toFixed(3);
+        }
+    }
+    printInverse(inverse);
 });
-
-
-// input_setup(3);
