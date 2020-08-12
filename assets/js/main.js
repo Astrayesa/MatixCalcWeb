2da51dfc3ba0d1893c2f87b69d956a3410b31b1d
Astrayesa <jsparrow0404@gmail.com>
2020-08-12 09:41:02 +0700

    ready to use

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 
-function printInverse(invarr){
+function printInverse(invarr) {
     let input_table = document.getElementById('output-table');
     input_table.innerHTML = "";
     for (let j = 0; j < 3; j++) {
@@ -13,32 +13,32 @@ function printInverse(invarr){
     }
 }
 
-function toidx(a, b){
+function toidx(a, b) {
     a += 1;
     b += 1;
     return 3 * (a - 1) + b - 1;
 }
 
-function deter(arr){
+function deter(arr) {
     det = arr[0] * arr[3] - arr[1] * arr[2];
     return det;
 }
 
 const inverse_button = document.getElementById("inverse-button");
-inverse_button.addEventListener('click', function(event){
+inverse_button.addEventListener('click', function (event) {
     let matrix_data = document.getElementsByClassName("matrix-data");
     let inputMatrix = new Array(3);
     let kofaktor = new Array(3);
-    for (let i = 0; i < 3; i++){
+    for (let i = 0; i < 3; i++) {
         inputMatrix[i] = new Array(3);
         kofaktor[i] = new Array(3);
-        for(let j = 0; j < 3; j++){
+        for (let j = 0; j < 3; j++) {
             inputMatrix[i][j] = parseInt(matrix_data[toidx(i, j)].value);
             minor = new Array(4);
             let ind = 0;
-            for(let k = 0; k < 3; k++){
-                for(let l = 0; l < 3; l++){
-                    if(!(k == i || l == j)){
+            for (let k = 0; k < 3; k++) {
+                for (let l = 0; l < 3; l++) {
+                    if (!(k == i || l == j)) {
                         minor[ind] = parseInt(matrix_data[toidx(k, l)].value);
                         ++ind;
                     }
@@ -48,38 +48,36 @@ inverse_button.addEventListener('click', function(event){
         }
     }
     let determinan = 0;
-    for(let i = 0; i < 3; i++){
+    for (let i = 0; i < 3; i++) {
         determinan += kofaktor[i][0] * matrix_data[toidx(i, 0)].value;
     }
 
     const inverse = new Array(3);
-    if(determinan == 0){
+    if (determinan == 0) {
         alert("Determinan = 0, maka matrix tersebut tidak memiliki inverse");
-        // const inverse = new Array(3);
-        for (let i = 0; i < 3; i++){
+        for (let i = 0; i < 3; i++) {
             inverse[i] = new Array(3);
-            for (let j = 0; j < 3; j++){
+            for (let j = 0; j < 3; j++) {
                 inverse[i][j] = '-';
             }
         }
     }
-    else{
-        // const inverse = new Array(3);
-        for (let i = 0; i < 3; i++){
+    else {
+        for (let i = 0; i < 3; i++) {
             inverse[i] = new Array(3);
-            for (let j = 0; j < 3; j++){
+            for (let j = 0; j < 3; j++) {
                 inverse[i][j] = (kofaktor[j][i] / determinan).toFixed(3);
             }
         }
     }
 
     const history = {
-        inputMatrix : inputMatrix,
-        determinan : determinan,
-        inverseMatrix : inverse
+        inputMatrix: inputMatrix,
+        determinan: determinan,
+        inverseMatrix: inverse
     }
 
     putHistory(history);
-
     printInverse(inverse);
+    renderHistory();
 });
--- assets/js/storage.js
+++ assets/js/storage.js
@@ -21,6 +21,9 @@ function putHistory(data) {
  
        localStorage.setItem(CACHE_KEY, JSON.stringify(historyData));
    }
+   else{
+       alert("browser tidak mendukung local storage")
+   }
 }
  
 function showHistory() {
@@ -30,18 +33,35 @@ function showHistory() {
        return [];
    }
 }
+
+function makeTable(arr){
+    let table = '<table>';
+    for(let i = 0; i < 3; i++){
+        table += '<tr>';
+        for(let j = 0; j < 3; j++){
+            table += '<td>' + arr[i][j] + '</td>';
+        }
+        table += '</tr>';
+    }
+    table += '</table>';
+    return table;
+}
  
 function renderHistory() {
    const historyData = showHistory();
-   let historyList = document.querySelector("#historyList");
+   let historyList = document.querySelector("#history-table");
    historyList.innerHTML = "";
+   let head = document.createElement('tr');
+   head.innerHTML = "<td>" + "Matrix" + "</td>";
+   head.innerHTML += "<td>" + "Determinan" + "</td>";
+   head.innerHTML += "<td>" + "Inverse" + "</td>";
+   historyList.appendChild(head);
  
    for (let history of historyData) {
        let row = document.createElement('tr');
-       row.innerHTML = "<td>" + history.firstNumber + "</td>";
-       row.innerHTML += "<td>" + history.operator + "</td>";
-       row.innerHTML += "<td>" + history.secondNumber + "</td>";
-       row.innerHTML += "<td>" + history.result + "</td>";
+       row.innerHTML = "<td>" + makeTable(history.inputMatrix) + "</td>";
+       row.innerHTML += "<td>" + history.determinan + "</td>";
+       row.innerHTML += "<td>" + makeTable(history.inverseMatrix) + "</td>";
  
        historyList.appendChild(row);
    }
